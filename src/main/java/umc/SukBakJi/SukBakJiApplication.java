4234a94f7aaf27b006e959ee7014048ed61d6c7a
plum-king <jeesoo0908@gmail.com>
2024-07-17 03:44:35 +0900

    Project Setting

---
+++ src/main/java/umc/SukBakJi/SukBakJiApplication.java
@@ -0,0 +1,13 @@
+package umc.SukBakJi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SukBakJiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SukBakJiApplication.class, args);
+	}
+
+}
