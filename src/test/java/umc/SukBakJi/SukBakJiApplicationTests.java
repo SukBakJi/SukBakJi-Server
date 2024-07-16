be9f4722be7400944678bb692dd15dafd396de0c
plum-king <jeesoo0908@gmail.com>
2024-07-17 03:44:35 +0900

    Project Setting

---
+++ src/test/java/umc/SukBakJi/SukBakJiApplicationTests.java
@@ -0,0 +1,13 @@
+package umc.SukBakJi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SukBakJiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
