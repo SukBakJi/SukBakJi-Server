b932a6cdb1373da7d9d24824be39a0342fa27b17
suzinlim <122861956+suzinlim@users.noreply.github.com>
2024-07-31 14:32:43 +0900

    #19 [Feat] 프로필 수정 API 구현

--- src/main/java/umc/SukBakJi/domain/repository/MemberResearchTopicRepository.java
+++ src/main/java/umc/SukBakJi/domain/repository/MemberResearchTopicRepository.java
@@ -1,7 +1,11 @@
 package umc.SukBakJi.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import umc.SukBakJi.domain.model.entity.Member;
 import umc.SukBakJi.domain.model.entity.mapping.MemberResearchTopic;
 
+import java.util.List;
+
 public interface MemberResearchTopicRepository extends JpaRepository<MemberResearchTopic, Long> {
+    List<MemberResearchTopic> findMemberResearchTopicByMember(Member member);
 }
