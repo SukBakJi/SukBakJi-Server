e75e0b313f9ce777f970f8f2dd59a2aeac3fb591
eunsxx <88652027+eunsxx@users.noreply.github.com>
2024-08-07 17:14:41 +0900

    #61 [Fix] 사용자 관심 연구주제 모아보기 응답 형식 수정

--- src/main/java/umc/SukBakJi/domain/controller/LabController.java
+++ src/main/java/umc/SukBakJi/domain/controller/LabController.java
@@ -5,7 +5,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import umc.SukBakJi.domain.model.dto.InterestTopicDTO;
+import umc.SukBakJi.domain.model.dto.InterestTopicsDTO;
 import umc.SukBakJi.domain.model.dto.LabDetailResponseDTO;
 import umc.SukBakJi.domain.model.dto.LabResponseDTO;
 import umc.SukBakJi.domain.model.dto.SearchLabRequestDTO;
@@ -57,10 +57,11 @@ public class LabController {
     @Operation(summary = "사용자의 관심 연구 주제 목록 조회", description = "사용자가 설정한 관심 주제 목록을 조회합니다.")
     @ApiResponses(value = {
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "성공"),
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404", description = "사용자를 찾을 수 없습니다.")
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404", description = "사용자를 찾을 수 없습니다."),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "500", description = "서버 에러, 관리자에게 문의 바랍니다.")
     })
     @GetMapping("/mypage/interest-topics")
-    public ResponseEntity<ApiResponse<List<InterestTopicDTO>>> getInterestTopicDTO(@RequestHeader("Authorization") String token) {
+    public ResponseEntity<ApiResponse<InterestTopicsDTO>> getInterestTopicDTO(@RequestHeader("Authorization") String token) {
 
         String jwt = token.substring(7);
 
@@ -68,7 +69,7 @@ public class LabController {
         Member member = memberRepository.findByEmail(email)
                 .orElseThrow(() -> new MemberHandler(ErrorStatus.MEMBER_NOT_FOUND));
 
-        List<InterestTopicDTO> topics = labService.getInterestTopics(member);
+        InterestTopicsDTO topics = labService.getInterestTopics(member);
 
         return ResponseEntity.ok(ApiResponse.onSuccess(topics));
     }
