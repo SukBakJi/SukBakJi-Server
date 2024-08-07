722b193091f0e9a39ba7f4a414137f4cab43b234
Woojin <wj05132@gmail.com>
2024-08-07 13:33:46 +0900

    #48[feat]: 연구실 검색, 상세보기 기능

--- src/main/java/umc/SukBakJi/domain/controller/LabController.java
+++ src/main/java/umc/SukBakJi/domain/controller/LabController.java
@@ -3,10 +3,12 @@ package umc.SukBakJi.domain.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import umc.SukBakJi.domain.model.dto.LabDetailResponseDTO;
 import umc.SukBakJi.domain.model.dto.LabResponseDTO;
 import umc.SukBakJi.domain.model.dto.SearchLabRequestDTO;
 import umc.SukBakJi.domain.service.LabService;
 import umc.SukBakJi.global.apiPayload.ApiResponse;
+import umc.SukBakJi.global.apiPayload.exception.GeneralException;
 
 import java.util.List;
 
@@ -25,5 +27,16 @@ public class LabController {
     public ResponseEntity<ApiResponse<List<LabResponseDTO>>> searchLabs(@RequestBody SearchLabRequestDTO searchRequest) {
         List<LabResponseDTO> labs = labService.searchLabsByTopicName(searchRequest.getTopicName());
         return ResponseEntity.ok(ApiResponse.onSuccess(labs));
+    }
+
+    @GetMapping("/{labId}")
+    public ResponseEntity<ApiResponse<LabDetailResponseDTO>> getLabDetail(@PathVariable Long labId) {
+        try {
+            LabDetailResponseDTO labDetail = labService.getLabDetail(labId);
+            return ResponseEntity.ok(ApiResponse.onSuccess(labDetail));
+        } catch (GeneralException e) {
+            return ResponseEntity.status(e.getErrorReasonHttpStatus().getHttpStatus())
+                    .body(ApiResponse.onFailure(e.getErrorReasonHttpStatus().getCode(), e.getErrorReasonHttpStatus().getMessage(), null));
+        }
     }
 }
