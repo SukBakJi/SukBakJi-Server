2f60de000927e13347ffd010baaa0aa6b6aa6397
suzinlim <122861956+suzinlim@users.noreply.github.com>
2024-08-09 22:32:57 +0900

    #74 [Feat] 연구 주제 검색 API 기능

--- src/main/java/umc/SukBakJi/domain/repository/ResearchTopicRepository.java
+++ src/main/java/umc/SukBakJi/domain/repository/ResearchTopicRepository.java
@@ -1,10 +1,17 @@
 package umc.SukBakJi.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import umc.SukBakJi.domain.model.entity.Lab;
 import umc.SukBakJi.domain.model.entity.ResearchTopic;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ResearchTopicRepository extends JpaRepository<ResearchTopic, Long> {
-    Optional<ResearchTopic> findByTopicName(String name);
+    @Query("SELECT rt FROM ResearchTopic rt WHERE rt.topicName = :topicName")
+    Optional<ResearchTopic> findByTopicName(String topicName);
+    @Query("SELECT rt FROM ResearchTopic rt WHERE rt.topicName LIKE %:topicName%")
+    List<ResearchTopic> findByTopicNameContaining(@Param("topicName") String topicName);
 }
