48daa9e2ee0ac8e03c7c4b5ae8fe1e997f74b37b
Sujin Lim <dkssudtnwls4794@naver.com>
2025-02-12 23:55:06 +0900

    [feat] 연구실 특정 대학 필터링 API 구현

--- src/main/java/umc/SukBakJi/domain/model/dto/LabResponseDTO.java
+++ src/main/java/umc/SukBakJi/domain/model/dto/LabResponseDTO.java
@@ -1,19 +1,29 @@
 package umc.SukBakJi.domain.model.dto;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.List;
 
-@Getter
-@Setter
-@Builder
 public class LabResponseDTO {
-    private Long labId;
-    private String labName;
-    private String universityName;
-    private String departmentName;
-    private String professorName;
-    private List<String> researchTopics;
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class LabPreviewResponseDTO {
+        private Long labId;
+        private String labName;
+        private String universityName;
+        private String departmentName;
+        private String professorName;
+        private List<String> researchTopics;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class LabSearchResponseDTO {
+        private List<LabPreviewResponseDTO> responseDTOList;
+        private int totalNumber;
+    }
 }
--- src/main/java/umc/SukBakJi/domain/repository/LabRepository.java
+++ src/main/java/umc/SukBakJi/domain/repository/LabRepository.java
@@ -1,5 +1,7 @@
 package umc.SukBakJi.domain.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -16,4 +18,5 @@ public interface LabRepository extends JpaRepository<Lab, Long> {
     Optional<Lab> findByUniversityNameAndProfessorNameAndDepartmentName(String universityName, String professorName, String departmentName);
     @Query("SELECT l FROM Lab l WHERE l.departmentName Like %:keyword%")
     List<Lab> findByDepartmentName(String keyword);
+    Page<Lab> findByUniversityName(String universityName, Pageable pageable);
 }
