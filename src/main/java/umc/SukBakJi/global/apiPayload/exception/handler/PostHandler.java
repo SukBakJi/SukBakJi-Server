d3754b0075adaf20455cf454b54e443ddc0e234f
suzinlim <122861956+suzinlim@users.noreply.github.com>
2025-04-11 22:08:06 +0900

    [fix] AOP 메소드 returning 타입 수정

--- src/main/java/umc/SukBakJi/global/apiPayload/exception/handler/PostHandler.java
+++ src/main/java/umc/SukBakJi/global/apiPayload/exception/handler/PostHandler.java
@@ -1,2 +1,10 @@
-package umc.SukBakJi.global.apiPayload.exception.handler;public class PostHandler {
+package umc.SukBakJi.global.apiPayload.exception.handler;
+
+import umc.SukBakJi.global.apiPayload.code.BaseErrorCode;
+import umc.SukBakJi.global.apiPayload.exception.GeneralException;
+
+public class PostHandler extends GeneralException {
+    public PostHandler(BaseErrorCode code) {
+        super(code);
+    }
 }
