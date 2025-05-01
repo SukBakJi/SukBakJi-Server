fbf635b7cf34c0360af5a80a7d1d9e38604090fc
suzinlim <122861956+suzinlim@users.noreply.github.com>
2025-05-02 04:36:04 +0900

    [refactor] 사용하지 않는 코드 제거

--- src/main/java/umc/SukBakJi/global/util/AppleJwtUtil.java
+++ src/main/java/umc/SukBakJi/global/util/AppleJwtUtil.java
@@ -12,7 +12,6 @@ import umc.SukBakJi.domain.auth.model.dto.AppleIdTokenPayload;
 import umc.SukBakJi.global.security.oauth2.service.AppleTokenValidator;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.security.KeyFactory;
